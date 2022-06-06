3f68d88631f370dd04fe5087c0b7bcb4c9575796
Leroy-netizen <leroythomas051@gmail.com>
2022-06-06 21:42:49 +0300

    Done.

--- index.js
+++ index.js
@@ -1,11 +1,35 @@
-function scuberGreetingForFeet(){
+function scuberGreetingForFeet(distance) {
   // Write your code here!
+  let result;
+
+  if (distance <= 400) {
+    result = "This one is on me!";
+  } else if (distance > 2000 && distance < 2500) {
+    result = "I will gladly take your thirty bucks.";
+  } else {
+    result = "No can do.";
+  }
+  return result;
+}
+// ============================== //
+
+// ===========second function ======//
+
+scuberGreetingForFeet(300);
+
+function ternaryCheckCity(city) {
+  // Write your code here!
+
+  return city === "NYC" ? "Ok, sounds good." : "No go.";
 }
 
-function ternaryCheckCity(){
-  // Write your code here!
-}
-
-function switchOnCharmFromTip(){
-  // Write your code here!
+function switchOnCharmFromTip(tip) {
+  switch (tip) {
+    case "generous":
+      return "Thank you so much.";
+    case "not as generous":
+      return "Thank you.";
+    default:
+      return "Bye.";
+  }
 }
